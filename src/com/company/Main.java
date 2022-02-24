7154a1883af703b1056386dccb7f5fe6ee0f3e64
katerinaivpetkova <katerinaivpetkova@gmail.com>
2022-02-24 16:32:52 +0200

    This is second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, Katerina Petkova");
+        System.out.println("Something Random");
 
     }
 }
