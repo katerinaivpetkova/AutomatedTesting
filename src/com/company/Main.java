361e57b2c0fbbdced079458576f552b007851b25
katerinaivpetkova <katerinaivpetkova@gmail.com>
2022-02-26 20:36:18 +0200

    Homework from Lecture 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Something Random трети");
+        int min;
+        min = 1;
+        int max;
+        max = 10;
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
 
+        if (randomNum > 5) {
+            System.out.println("The number is greather than five. It is " + randomNum + ".");
+        }
     }
 }
